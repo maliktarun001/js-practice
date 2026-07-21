04dc0f6321ea231dd33ef32c5fcc27f18b2f96dc
tarunmalik330 <maliktarun2005@gmail.com>
2026-07-21 12:35:57 +0530

    Add initial files for variables, data types, and test logging

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,16 @@
+const accoountId = 144553
+let accountEmail= "maliktarun2005@gmail.com"
+let accountPassword = "12345"
+let accountCIty = "Jaipur"
+let accountState;
+// accoountId= 12 not allowed
+console.log(accoountId)
+accountEmail="user@gmail.com"
+accountPassword="123456"
+accountCIty="Delhi"
+console.table([accountEmail, accountPassword, accountCIty, accountState])
+
+// {} = scope
+/*
+prefer not to use var because of issue in block scope and functional scope
+*/
---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,16 @@
+// alert (3+3) we are using nodejs not browser
+// data type
+// number => 2 to power of 53
+// bigint => 1234567890123456789012345678901234567890n
+// string => ""
+// boolear => true or false
+// null => standalone value
+// undefined => value assigned to a variable that has been declared but not initialized
+// symbol => unique and immutable primitive value
+
+
+
+// object
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
