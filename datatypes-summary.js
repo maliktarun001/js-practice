51e975f0d759ac33cb0f5231a7682be0bbb09320
tarunmalik330 <maliktarun2005@gmail.com>
2026-07-23 14:40:12 +0530

    Update .cron-lock and enhance datatypes-summary with additional examples and type checks

--- datatypes-summary.js
+++ datatypes-summary.js
@@ -6,7 +6,17 @@
 // 7types of primitive datatypes
 // number, string, boolean, null, undefined, symbol, bigint
 // bigint
-// const bigNumber = 1234567890234567890n
+const bigNumber = 1234567890234567890n
 
 // Referce (non-primitive) datatypes
 // Aray object, function
+const heros = ["shaktiman", "naagraj", "logo"]
+let myObj = {
+    name: "tarun",
+    age: "20"
+}
+const myFunction = function(){
+    console.log('object')
+}
+console.log(typeof bigNumber)
+console.log(typeof myFunction)
