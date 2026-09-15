f36341e5ddb9d23b35e43ccd34488e1dc4bc8546
tarunmalik330 <maliktarun2005@gmail.com>
2026-09-15 19:35:43 +0530

    Add variable and data type examples in two.js; include reassignment, type checking, and array creation. Create three.js for a for loop demonstration.

---
+++ questions_practice/two.js
@@ -0,0 +1,52 @@
+// # Variables and data types
+// 1. Declare a variables using let and log its values.
+let a = 12;
+console.log(a)
+
+// 2 create a constant to store  the value of PI and log it.
+const Pi = Math.PI;
+console.log(Pi)
+
+// 3 Reassign a value to a variable  declare with let and log  the result.
+
+let valueOne = 32;
+valueOne = 24;
+console.log(valueOne)
+
+// 4 check the type of null  and log it.
+console.log(typeof null)
+
+// 5 create a variables with a number as a string (e.g., 25) and log its type.
+var number = "25"
+console.log(typeof number)
+
+// 6 use typeof to check the type of a boolean variable.
+const myType = true
+console.log(typeof myType)
+
+// 7  create three variables of types string, number, and boolean, and log their values.
+
+var varOne = "name";
+var age = 20;
+var N = true;
+console.log(typeof varOne, age, N)
+
+// 8 declare a variable without assigning a value. Log its type.
+let assignValue
+console.log(assignValue)
+
+// 9 create a variable with undefined and log its type.
+var abc = undefined
+console.log(abc)
+
+// 10 use to create an array. Try reassigning the array and observe the error.
+const myArray = [1, 2, 3, 4, 5, 6, 7];
+// myArray = ["ram", "shyam"]
+// in array we not ressign but we can update array values
+console.log(myArray)
+
+// 12 = number
+// harsh = String
+// true false = Boolean
+// NaN = number
+// null = Object
